180793a41f5b439c43cfb52c47a472ccbe4c4907
licanchen <licanchen430@163.com>
2018-07-22 19:55:35 +0800

    Tab Bar can be hidden

---
+++ Art Pieces/LecturePresentationViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LectureViewController.swift
+//  Art Pieces
+//
+//  Created by 李灿晨 on 2018/7/22.
+//  Copyright © 2018 李灿晨. All rights reserved.
+//
+
+import UIKit
+
+class LecturePresentationViewController: UIViewController {
+
+    @IBOutlet weak var stepTableView: UITableView!
+    @IBOutlet weak var artworkView: ArtworkView!
+    
+    var selectedSteps: Set<Int> = []
+    
+    var artworkGuide: ArtworkGuide = ArtworkGuide()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Only for test use
+        var step = Step()
+        let renderMechanism = RenderMechanism(color: .black, width: 1.0)
+        let subStep = SubStep(operationType: .colorChange, renderMechanism: renderMechanism,
+                              renderDescription: "Step 1")
+        step.add(subStep: subStep)
+        step.add(subStep: subStep)
+        step.description = "Step 1"
+        artworkGuide.add(step: step)
+        step.description = "Step 2"
+        artworkGuide.add(step: step)
+        step.description = "Step 3"
+        artworkGuide.add(step: step)
+        
+        // initialize the cell
+        stepTableView.register(StepTableViewCell.self, forCellReuseIdentifier: "stepTableViewCell")
+        stepTableView.reloadData()
+    }
+
+}
+
+
+extension LecturePresentationViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artworkGuide.steps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let stepTableViewCell = StepTableViewCell()
+        stepTableViewCell.selectionStyle = .none
+        stepTableViewCell.delegate = self
+        stepTableViewCell.backgroundColor = UIColor(red: 247/255, green: 246/255, blue: 244/255, alpha: 1)
+        stepTableViewCell.clipsToBounds = true
+        stepTableViewCell.step = artworkGuide.steps[indexPath.row]
+        stepTableViewCell.index = indexPath.row
+        if selectedSteps.contains(indexPath.row) {
+            stepTableViewCell.setupDetailedInterface()
+        } else {
+            stepTableViewCell.setupTitleInterface()
+        }
+        return stepTableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if selectedSteps.contains(indexPath.row) {
+            let step = artworkGuide.steps[indexPath.row]
+            let fullHeight = CGFloat(step.subSteps.count) * SubStepView.height + StepTitleView.height
+            return fullHeight
+        } else {
+            let titleHeight = StepTitleView.height
+            return titleHeight
+        }
+    }
+    
+}
+
+extension LecturePresentationViewController: StepTableViewCellDelegate {
+    
+    func stepTitleBarDidTapped(at index: Int) {
+        stepTableView.beginUpdates()
+        if selectedSteps.contains(index) {
+            selectedSteps.remove(index)
+        } else {
+            selectedSteps.insert(index)
+        }
+        stepTableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+        stepTableView.endUpdates()
+    }
+    
+    func subStepInteractionButtonDidTapped(at index: Int) {
+    }
+    
+}
--- Art Pieces/LectureViewController.swift
+++ Art Pieces/LectureViewController.swift
@@ -10,88 +10,32 @@ import UIKit
 
 class LectureViewController: UIViewController {
 
-    @IBOutlet weak var stepTableView: UITableView!
-    @IBOutlet weak var artworkView: ArtworkView!
-    
-    var selectedSteps: Set<Int> = []
-    
-    var artworkGuide: ArtworkGuide = ArtworkGuide()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Only for test use
-        var step = Step()
-        let renderMechanism = RenderMechanism(color: .black, width: 1.0)
-        let subStep = SubStep(operationType: .colorChange, renderMechanism: renderMechanism,
-                              renderDescription: "Step 1")
-        step.add(subStep: subStep)
-        step.add(subStep: subStep)
-        step.description = "Step 1"
-        artworkGuide.add(step: step)
-        step.description = "Step 2"
-        artworkGuide.add(step: step)
-        step.description = "Step 3"
-        artworkGuide.add(step: step)
-        
-        // initialize the cell
-        stepTableView.register(StepTableViewCell.self, forCellReuseIdentifier: "stepTableViewCell")
-        stepTableView.reloadData()
-        
     }
+    
+    @IBAction func pushButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "showLecturePresentationViewController", sender: nil)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        switch segue.identifier {
+//        case "showLecturePresentationViewController":
+//            let destination = segue.destination as! LecturePresentationViewController
+//            destination.hidesBottomBarWhenPushed = true
+//        default:
+//            break
+//        }
+//    }
+    
+    /*
+    // MARK: - Navigation
 
-}
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
-
-extension LectureViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return artworkGuide.steps.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let stepTableViewCell = StepTableViewCell()
-        stepTableViewCell.selectionStyle = .none
-        stepTableViewCell.delegate = self
-        stepTableViewCell.backgroundColor = UIColor(red: 247/255, green: 246/255, blue: 244/255, alpha: 1)
-        stepTableViewCell.clipsToBounds = true
-        stepTableViewCell.step = artworkGuide.steps[indexPath.row]
-        stepTableViewCell.index = indexPath.row
-        if selectedSteps.contains(indexPath.row) {
-            stepTableViewCell.setupDetailedInterface()
-        } else {
-            stepTableViewCell.setupTitleInterface()
-        }
-        return stepTableViewCell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if selectedSteps.contains(indexPath.row) {
-            let step = artworkGuide.steps[indexPath.row]
-            let fullHeight = CGFloat(step.subSteps.count) * SubStepView.height + StepTitleView.height
-            return fullHeight
-        } else {
-            let titleHeight = StepTitleView.height
-            return titleHeight
-        }
-    }
-    
-}
-
-extension LectureViewController: StepTableViewCellDelegate {
-    
-    func stepTitleBarDidTapped(at index: Int) {
-        stepTableView.beginUpdates()
-        if selectedSteps.contains(index) {
-            selectedSteps.remove(index)
-        } else {
-            selectedSteps.insert(index)
-        }
-        stepTableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-        stepTableView.endUpdates()
-    }
-    
-    func subStepInteractionButtonDidTapped(at index: Int) {
-    }
-    
 }
